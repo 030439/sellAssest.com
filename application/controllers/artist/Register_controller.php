a3c276c0b16abb6193d9cc83f5a56caac0e61f0f
030439 <praimchand763@gmail.com>
2022-11-10 17:58:40 +0500

    register controller created

---
+++ application/controllers/artist/Register_controller.php
@@ -0,0 +1,9 @@
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Register_Controller extends CI_Controller{
+    public function __consruct(){
+        parent::__consruct();
+        $this->load->helper('form');
+        $this->load->library('form_validation');
+    }
+}
